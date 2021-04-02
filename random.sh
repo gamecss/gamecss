ae5d48dfa7d98efb73a5ce9b5b096d818305b4d4
gamecss <52902973+gamecss@users.noreply.github.com>
2021-04-02 12:14:08 +0800

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-s1='无话可说  '
-s2='干啥啥不行 删库第一名  '
-s3='fucking gfw  '
-s4='瞅啥瞅?  '
-s5='学业繁重......  '
-rndm="s$((`date +%s%N`%5+1))"
+s1='无话可说'
+s2='干啥啥不行 删库第一名'
+s3='fucking gfw'
+s4='瞅啥瞅?'
+s5='学业繁重......'
+s6='小母牛搞开源———牛到github上了'
+s7='混吃等死'
+
+rndm="s$((`date +%s%N`%7+1))"
 says=`eval echo '$'"$rndm"`
-echo $says
+echo -n $says
